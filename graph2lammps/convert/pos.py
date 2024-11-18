80a94062b1e4c88ae7560a0d97c2f9063a9a48a1
Shengli Jiang <sj0161@della8.princeton.edu>
2024-11-18 13:52:59 -0500

    code update

--- graph2lammps/convert/pos.py
+++ graph2lammps/convert/pos.py
@@ -42,62 +42,63 @@ def gen_pos(G, rmin, bdist, bmin, bmax, niter):
     """
     N    = G.number_of_nodes()
     rcut = rmin / 0.8
-    
+
     # Initial positions using Kamada-Kawai path-length cost-function.
+    np.random.seed(0)
     pos  = nx.kamada_kawai_layout(G, dim=3)
+    pos = nx.spring_layout(G, pos=pos, iterations=500, dim=3)
     pos  = np.vstack([pos[key] for key in sorted(pos.keys())])
     adj  = nx.adjacency_matrix(G).toarray()
-    
+
     bonded = adj == 1
     angled = np.matmul(adj, adj) > 0
-    
+
     for step in range(niter):
-        print(pos[0])
         r    = pos[:, np.newaxis, :] - pos[np.newaxis, :, :]
         dist = np.sqrt(np.sum(r ** 2, axis=-1))
-        
+
         # Any significant overlap amongst any particles.
         big_overlap = dist < rmin
         big_overlap[bonded] = False
         big_overlap[np.diag_indices(N)] = False
-        
+
         # Any significant stretching and shrinking of the bonds.
         bad_bond = np.logical_or(dist[bonded] < bmin, dist[bonded] > bmax)
-        
+
         if not big_overlap.any() and not bad_bond.any():
             print(f"# No substantial overlaps or bad bonds found after {step} steps...\n\n")
             break
         else:
             if step % 50 == 0:
                 print(f"# Significant overlaps or bad bonds detected. Attempting pushoff step {step+1}")
-    
+
         # Force Initialization
         fmag = np.zeros([N, N])
         f    = np.zeros([N, N, 3])
-        
+
         # Compute all pairwise forces using a soft-core potential Uij = A[a+ cos(pi*rij/rc)]
         # Find elements where the distance matrix is less than the cutoff
-        
+
         small_overlap = dist < rcut
         small_overlap[bonded] = False
         small_overlap[np.diag_indices(N)] = False
-        
+
         scale = 0.1
-            
+
         fmag[small_overlap] = scale * np.sin(np.pi * dist[small_overlap] / rcut) / rcut / dist[small_overlap]
-        
+
         # Compute all bonded forces using a harmonic potential Uij = K(d-d0)^2
         fmag[bonded] = - scale * (dist[bonded] - bdist) / dist[bonded]
-        
+
         # TODO: Angle force
-        
+
         # Compute vector force
         f =  r * fmag[:, :, None]
-        
+
         # Get sum of froce on each particle
         fi = np.sum(f, axis=1)
-        
+
         # Evolve positions
         pos += 0.5 * fi
-    
+
     return pos
--- graph2lammps/multi/gen_data.py
+++ graph2lammps/multi/gen_data.py
@@ -1,84 +1,79 @@
 import numpy as np
 import itertools
-from graph2lammps.convert import read_pdb
+
 
 def rotate_smallest_volume(pos):
-    cov      = np.cov(pos, rowvar=False)
+    cov = np.cov(pos, rowvar=False)
     lam, vec = np.linalg.eigh(cov)
-    idx      = np.argsort(lam)[::-1]
-    vec      = vec[:, idx]
-    pos_out  = np.dot(pos, vec)
+    idx = np.argsort(lam)[::-1]
+    vec = vec[:, idx]
+    pos_out = np.dot(pos, vec)
     return pos_out
+
 
 def uniform_sample(vmin, vmax, n):
     ranges = vmax - vmin
     total_points = np.prod(ranges)
     n = min(n, total_points)
-    
+
     # Generate uniform samples along each axis
-    samples = [np.linspace(vmin[i], vmax[i], int(ranges[i]), endpoint=False) for i in range(len(vmin))]
-    
+    samples = [
+        np.linspace(vmin[i], vmax[i], int(ranges[i]), endpoint=False)
+        for i in range(len(vmin))
+    ]
+
     indices = np.random.choice(total_points, n, replace=False)
-    
-    points = np.array([samples[i][indices % int(ranges[i])] for i in range(len(vmin))]).T
-    
+
+    points = np.array(
+        [samples[i][indices % int(ranges[i])] for i in range(len(vmin))]).T
+
     return points.astype('int')
+
 
 def get_mw(npoly, nbead, mbead, nsolv, msolv):
     return (npoly * nbead * mbead) / (nsolv * msolv + npoly * nbead * mbead)
 
+
 def get_rho(npoly, nbead, L):
-    return npoly * nbead / L ** 3
+    return npoly * nbead / L**3
+
 
 def gen_data(pos, args):
-    if args.pdb != None:
-        nbead = len(pos)
-        mbead = 5.0         # mass polymer bead
-        msolv = 1.0         # mass solvent
-        L     = int(5 * nbead ** 0.5 + 0.5) # cubic simulation box side length
-        nsolv = 5 * L ** 3  # fixed number density of solvent for neutral buoyancy 
-        npoly = L ** 3 / ((1-args.rho) / args.rho) / nbead # number of polymer chains 2 wt%
-        npoly = int(npoly + 0.5)
-        pos_out = read_pdb(args.pdb)
-        vmax  = np.max(np.abs(pos_out))
-        pos_out = pos_out * (L / 2 / vmax)
-        print(f"# Num poly: {npoly}")
-    
-        return pos_out, L, nsolv, npoly
-        
-    pos   = rotate_smallest_volume(pos) # smallest volume
-        
-    vmin  = pos.min(axis=0)
-    vmax  = pos.max(axis=0)
-    size  = vmax - vmin + .2 # e.g., [1.2, 3.1, 2.3] for height, width, and depth, add 0.5, so they don't touch
-    
+    pos = rotate_smallest_volume(pos)  # smallest volume
+
+    vmin = pos.min(axis=0)
+    vmax = pos.max(axis=0)
+    size = vmax - vmin + .2  # e.g., [1.2, 3.1, 2.3] for height, width, and depth, add 0.5, so they don't touch
+
     nbead = len(pos)
-    mbead = 5.0         # mass polymer bead
-    msolv = 1.0         # mass solvent
-    L     = int(5 * nbead ** 0.5 + 0.5) # cubic simulation box side length
-    nsolv = 5 * L ** 3  # fixed number density of solvent for neutral buoyancy 
-    npoly = L ** 3 / ((1-args.rho) / args.rho) / nbead # number of polymer chains 2 wt%
+    mbead = 5.0  # mass polymer bead
+    msolv = 1.0  # mass solvent
+    L = 75  # int(5 * nbead ** 0.5 + 0.5) # cubic simulation box side length
+    nsolv = 5 * L**3  # fixed number density of solvent for neutral buoyancy
+    npoly = L**3 / (
+        (1 - args.rho) / args.rho) / nbead  # number of polymer chains 2 wt%
     npoly = int(npoly + 0.5)
-    
-    lcube = (L / size).astype('int') 
-    ncube = np.prod(lcube)  # number of single polymer cubes fit in simulation box
+
+    lcube = (L / size).astype('int')
+    ncube = np.prod(
+        lcube)  # number of single polymer cubes fit in simulation box
     # shrink the original polymer size if necessary
-    alpha = 1.0 # shrinking factor
+    alpha = 1.0  # shrinking factor
     new_size = np.copy(size)
     while ncube < npoly and alpha > 0.02:
         new_size = size * alpha
-        lcube    = (L / new_size).astype('int')
-        ncube    = np.prod(lcube)
-        alpha    = alpha - 0.01
-        
+        lcube = (L / new_size).astype('int')
+        ncube = np.prod(lcube)
+        alpha = alpha - 0.01
+
     if alpha == 1.0:
         new_size = size
     else:
         alpha = alpha + 0.01
-        
+
     if alpha < 0.02:
         raise RuntimeError("# Shrinking factor too small ...")
-    
+
     print(f"# Box size: {L}x{L}x{L}")
     print(f"# Cube    : {lcube[0]}x{lcube[1]}x{lcube[2]}")
     print(f"# N Cube  : {ncube}")
@@ -87,25 +82,27 @@ def gen_data(pos, args):
     print(f"# Num solv: {nsolv}")
     print(f"# Poly wt : {get_mw(npoly, nbead, mbead, nsolv, msolv):0.6f}")
     print(f"# Poly rho: {get_rho(npoly, nbead, L):0.6f}")
-    
-    pos_new  = np.copy(pos)
-    pos_new  = pos * alpha
+
+    pos_new = np.copy(pos)
+    pos_new = pos * alpha
     vmin_new = pos_new.min(axis=0)
-    pos_new   = pos_new - vmin_new
-    pos_new   = pos_new - L / 2
-    pos_out  = []
+    pos_new = pos_new - vmin_new
+    pos_new = pos_new - L / 2
+    pos_out = []
     count = 0
-    for i, j, k in itertools.product(range(lcube[0]), range(lcube[1]), range(lcube[2])):
+
+    for i, j, k in itertools.product(range(lcube[0]), range(lcube[1]),
+                                     range(lcube[2])):
         pos_temp = np.copy(pos_new)
         pos_temp[:, 0] += i * new_size[0]
         pos_temp[:, 1] += j * new_size[1]
         pos_temp[:, 2] += k * new_size[2]
-        pos_temp       += np.random.random((pos.shape)) * 0.001
+        pos_temp += np.random.random((pos.shape)) * 0.001
         pos_out.append(pos_temp)
         count += 1
         if count == npoly:
             break
-    
+
     # points = uniform_sample(vmin=np.array([0, 0, 0]), vmax=lcube, n=npoly)
     # for p in points:
     #     pos_temp = np.copy(pos_new)
@@ -113,7 +110,5 @@ def gen_data(pos, args):
     #     pos_temp[:, 1] += p[1] * size[1]
     #     pos_temp[:, 2] += p[2] * size[2]
     #     pos_out.append(pos_temp)
-        
+
     return pos_out, L, nsolv, npoly
-     
-    
